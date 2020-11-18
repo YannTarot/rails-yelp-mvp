fb94caa9ac1be1b9f4b3f59f5357a535d3547795
Yann Tarot <yann_tarot@yahoo.fr>
2020-11-18 15:36:21 +0100

    Yelp

--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -1,5 +1,5 @@
 class Restaurant < ApplicationRecord
-   CATEGORIES = %w[chinese italian japanese french belgian]
+  CATEGORIES = %w[chinese italian japanese french belgian]
 
   validates :name, presence: true
   validates :address, presence: true
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,19 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+puts "Cleaning database..."
+Restaurant.destroy_all
+
+puts "Creating restaurants..."
+mcdonald = { name: "McDonald", address: "18 rue du Fast food, Nantes", phone_number: "06459876462" ,category: "belgian" }
+pizza_yolo =  { name: "Pizza Yolo", address: "4 boulevard Pepperoni, Bordeaux", phone_number: "0746589548", category: "italian" }
+sakura = { name: "Sakura", address: "65 rue du Wasabi, Paris", phone_number: "06329784265", category: "japanese" }
+luigi_pasta =  { name: "Luigi's pasta", address: "45 avenue de la Sauce Bolognaise, Rennes", phone_number: "0648794614", category: "italian" }
+kaboul_kebab =  { name: "Kaboul Kebab", address: "98 rue Sauce Samourai, Lyon", phone_number: "0612345678", category: "french" }
+
+
+[ mcdonald, pizza_yolo, sakura, luigi_pasta, kaboul_kebab].each do |attributes|
+  restaurant = Restaurant.create!(attributes)
+  puts "Created #{restaurant.name}"
+end
+puts "Finished!"
